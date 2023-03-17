52661ddf9e853d50253e3c381ebcbc61a6261211
mohitrajrathor <mohitrajrathor@gmail.com>
2023-03-17 23:36:22 +0530

    change django version from django~=4.x to django~=3.2.10

--- fantasysite/settings.py
+++ fantasysite/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'G6Dg756@my&*#fh"][75--g.??gije=-*/-rw7324sfrewg'
 SECRET_KEY = config('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
